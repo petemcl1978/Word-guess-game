83f84b15a32805c12d69e1dcb31a23ca00d92459
Pete McLaughlin <Petemcl1978@gmail.com>
2019-04-11 12:29:23 -0400

    oh yeah beast mode

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var name = prompt("whats your name?");
+console.log("hello" + name);
+
+//create an array of words
+var words = ["The Dude", "Walter", "Donnie", "Jackie Treehorn", "the ringer", "Caucasian", "Rug", "Pomeranian", "Nihilist", "Bowling", "Bunny", "Lebowski", "The Jesus", "Maude"];
+
+// pick a random word from the words array
+var word = words[Math.floor(math.random() * words.length];
+// set up the answerArray to show how many letters being used
+Var answerArray =[];
+for (var i = 0; i <word.length; i++){
+    answerArray [i] = "_";
+}
+//create a variable to hold the number of remaining letters 
+var remaningLetters = word.length;
+
+// ******* main game loop************
