a4cc580f130ad4ab05e2f09ceb29dd8cad1a0180
Pete McLaughlin <Petemcl1978@gmail.com>
2019-04-11 13:46:37 -0400

    change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,9 @@ console.log("hello" + name);
 var words = ["The Dude", "Walter", "Donnie", "Jackie Treehorn", "the ringer", "Caucasian", "Rug", "Pomeranian", "Nihilist", "Bowling", "Bunny", "Lebowski", "The Jesus", "Maude"];
 
 // pick a random word from the words array
-var word = words[Math.floor(math.random() * words.length];
+var word = words[ Math.floor(math.random() * words.length)];
 // set up the answerArray to show how many letters being used
-Var answerArray =[];
+var answerArray =[];
 for (var i = 0; i <word.length; i++){
     answerArray [i] = "_";
 }
@@ -15,3 +15,8 @@ for (var i = 0; i <word.length; i++){
 var remaningLetters = word.length;
 
 // ******* main game loop************
+while (remainingLetters > 0){
+    //showthe player their progress 
+    alert(answerArray.join(" "));
+
+}
