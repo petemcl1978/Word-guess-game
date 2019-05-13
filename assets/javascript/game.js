842c70204a90211e80991381dd3d366682a1203e
Pete McLaughlin <Petemcl1978@gmail.com>
2019-05-12 21:23:56 -0400

    making some chnages so it is not alerts anymore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,65 @@
+window.onload = function(){
+    var alphabet =['a','b', 'c', 'd', 'e','f', 'g','h','i', 'j', 'k',
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var word;
+var guess;
+var guesses =[];
+var lives; 
+var counter;
+var space;
+
+//get elements
+var showLives = document.getElementById("mylives");
+//create alphabet ul 
+var buttons= function() {
+    myButtons= document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i =0; i < alphabet.length; i++){
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        List.innerHTML = alphabet[i];
+        check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+    }
+}
+}
+//create guesses
+result = function() {
+    wordHolder = document.getElementById('hold');
+correct= document.createElement('ul');
+    for (var i =0; i < word.length; i++){
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+        if (word[i]=== "-"){
+            guess.innerHTML = "-";
+            space = 1;
+        } else {
+            guess.innerHTML = "-";
+        }
+        guesses.push(guess);
+        wordHolder.appendChild(corret);
+    }
+}
+//show lives
+comments = function () {
+    showlives.innerHTML = "you have " +
+    lives + "lives";
+    if (lives < 1) {
+        showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < guesses.length;i++){
+        if(counter + space ==== guesses.length){
+            show.lives.innerHTML = "That Really Ties the Room Together";
+        }
+    }
+}
+//onClick Function 
+
+
 // Create an array of words
 var words = [
     "lebowski",
@@ -34,14 +96,14 @@ var words = [
     // The game loop
     while (remainingLetters > 0) {
     // Show the player their progress
-    alert (answerArray.join(" "));
+    modal (answerArray.join(" "));
     // Get a guess from the player
     var guess = prompt("Guess a letter, or click Cancel to stop playing.");
     if (guess === null) {
     // Exit the game loop
     break;
     } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
+    modal ("Please enter a single letter.");
     } else {
     // Update the game state with the guess
     for (var j = 0; j < word.length; j++) {
